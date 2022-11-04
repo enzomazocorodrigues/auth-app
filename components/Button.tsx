07643062fb253cf541dc6509ee51aaa0bb05127e
enzomazocorodrigues <enzo.rodrigues2@gmail.com>
2022-11-04 12:02:23 -0300

    feat: add asChild prop on Button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,14 +1,18 @@
+import { Slot } from "@radix-ui/react-slot";
 import { ButtonHTMLAttributes } from "react";
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  asChild?: boolean;
+};
 
-export function Button({ children, ...props }: ButtonProps) {
+export function Button({ asChild, children, ...props }: ButtonProps) {
+  const Comp = asChild ? Slot : "button";
   return (
-    <button
-      className="w-full bg-green-400 p-3 rounded-md text-base font-medium text-white"
+    <Comp
+      className="w-full text-center bg-green-400 p-3 rounded-md text-base font-medium text-white hover:bg-green-300 focus:ring-2 outline-none"
       {...props}
     >
       {children}
-    </button>
+    </Comp>
   );
 }
