97459143d1c7cc6771963aae063a950f30328ad7
enzomazocorodrigues <enzo.rodrigues2@gmail.com>
2022-11-04 12:03:59 -0300

    feat: add large prop on PageIcon component

--- components/PageIcon.tsx
+++ components/PageIcon.tsx
@@ -3,11 +3,16 @@ import { ReactNode } from "react";
 
 type PageIconProps = {
   children: ReactNode;
+  large?: boolean;
 };
 
-export function PageIcon({ children }: PageIconProps) {
+export function PageIcon({ children, large }: PageIconProps) {
   return (
-    <div className="w-12 h-12 rounded-full bg-green-100 flex justify-center items-center">
+    <div
+      className={`${
+        large ? "w-24 h-24" : "w-12 h-12"
+      } rounded-full bg-green-100 flex justify-center items-center`}
+    >
       <Slot itemProp="width" className="text-green-500">
         {children}
       </Slot>
