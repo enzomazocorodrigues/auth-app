407290af699b8549961da97fda06630deffe5ff6
enzomazocorodrigues <enzo.rodrigues2@gmail.com>
2022-11-09 10:15:34 -0300

    refactor: change page routes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,24 @@
-import { KeyIcon } from "@heroicons/react/24/solid";
+import { CheckIcon } from "@heroicons/react/24/solid";
 import { Button } from "../components/Button";
-import { Input } from "../components/Input";
 import { PageIcon } from "../components/PageIcon";
 
 export default function Home() {
   return (
     <div className="w-full h-screen bg-slate-100 flex justify-center items-center p-10">
-      <div className="w-[426px] bg-white drop-shadow-sm rounded-md p-6 flex flex-col gap-6">
-        <div className="flex gap-4 items-center">
-          <PageIcon>
-            <KeyIcon width={28} height={28} />
-          </PageIcon>
-          <h1 className="text-2xl text-slate-700 font-bold">Sign in</h1>
-        </div>
+      <div className="w-[426px] bg-white drop-shadow-sm rounded-md p-6 flex flex-col gap-6 items-center">
+        <PageIcon large>
+          <CheckIcon width={56} height={56} />
+        </PageIcon>
+        <h1 className="text-2xl text-slate-700 font-bold w-3/4 text-center">
+          Congratulations Enzo, you’re in! 🎉
+        </h1>
         <h2 className="text-xs font-medium text-slate-400">
           Cras sed felis eget velit aliquet sagittis id consectetur purus ut
           faucibus pulvinar.
         </h2>
-        <hr className="w-full" />
-        <Input label="Email" id="email" type="email" />
-        <Input label="Password" id="password" type="password" />
         <Button asChild>
-          <a href="/success">Sign in</a>
+          <a href="/signin">Log out</a>
         </Button>
-        <hr className="w-full" />
-        <span className="text-center text-sm text-slate-600 font-medium">
-          Don’t have an account?
-          <a
-            className="ml-1 text-blue-500 outline-none focus:ring-2 rounded-sm hover:text-blue-400"
-            href="/signup"
-          >
-            Sign up
-          </a>
-        </span>
       </div>
     </div>
   );
--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,38 +1,36 @@
-import { DocumentCheckIcon } from "@heroicons/react/24/solid";
+import { KeyIcon } from "@heroicons/react/24/solid";
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
 import { PageIcon } from "../components/PageIcon";
 
-export default function SignUp() {
+export default function SignIn() {
   return (
-    <div className="w-full h-full bg-slate-100 flex justify-center items-center p-14">
+    <div className="w-full h-screen bg-slate-100 flex justify-center items-center p-10">
       <div className="w-[426px] bg-white drop-shadow-sm rounded-md p-6 flex flex-col gap-6">
         <div className="flex gap-4 items-center">
           <PageIcon>
-            <DocumentCheckIcon width={28} height={28} />
+            <KeyIcon width={28} height={28} />
           </PageIcon>
-          <h1 className="text-2xl text-slate-700 font-bold">Sign up</h1>
+          <h1 className="text-2xl text-slate-700 font-bold">Sign in</h1>
         </div>
         <h2 className="text-xs font-medium text-slate-400">
           Cras sed felis eget velit aliquet sagittis id consectetur purus ut
           faucibus pulvinar.
         </h2>
         <hr className="w-full" />
-        <Input label="Name" id="name" />
         <Input label="Email" id="email" type="email" />
         <Input label="Password" id="password" type="password" />
-        <Input label="Confirm password" id="confirm-password" type="password" />
         <Button asChild>
-          <a href="/signin">Sign up</a>
+          <a href="/">Sign in</a>
         </Button>
         <hr className="w-full" />
         <span className="text-center text-sm text-slate-600 font-medium">
-          Already have an account?
+          Don’t have an account?
           <a
             className="ml-1 text-blue-500 outline-none focus:ring-2 rounded-sm hover:text-blue-400"
-            href="/signin"
+            href="/signup"
           >
-            Sign in
+            Sign up
           </a>
         </span>
       </div>
